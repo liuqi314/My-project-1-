0a01718dd4a4323fef3243d586a993b282f715d8
liuqi314 <w0490374@nscc.ca>
2023-11-06 21:12:40 -0400

    Add Version Number
    Add Version Number,
    Add Code-Version Number

---
+++ Assets/Scripts/Versioninfo.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    private Text _versionNumberText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _versionNumberText = GetComponent<Text>();
+        _versionNumberText.text = "VERSION:" + Application.version;
+
+    }
+
+  
+}
